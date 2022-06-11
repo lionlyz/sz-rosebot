6cbf744d1a2a77bbb9dc500ffe47d43a4a49e67f
dimas <72862552+divmass@users.noreply.github.com>
2022-06-11 19:02:51 +0800

    Update welcome.py

--- Rose/plugins/welcome.py
+++ Rose/plugins/welcome.py
@@ -210,7 +210,7 @@ my news channel @Theszrosebot.
                 "Wow ! Owner has just joined your chat.",
             )
                return
-        if member.id == 1467358214:#for @supunma 
+        if member.id == 2022313646:#for @divmas 
                await app.send_message(
                 message.chat.id,
                 "Wow ! Developer has just joined your chat.",
@@ -404,19 +404,19 @@ async def goodbye(client, message: Message, _):
     await app.send_message(message.chat.id, text=tek, reply_markup=button)
     return
 
-__MODULE__ = f"{Greeting}"
+__MODULE__ = f"{Welcome}"
 __HELP__ = """
 Give your members a warm welcome with the greetings module! Or a sad goodbye... Depends!
 
 **Admin commands:**
-- /welcome `<yes/no/on/off>`: Enable/disable welcomes messages.
-- /goodbye `<yes/no/on/off>`: Enable/disable goodbye messages.
+- /welcome `<yes/no/on/off>`: ON|OFF welcomes messages.
+- /goodbye `<yes/no/on/off>`: ON|OFF goodbye messages.
 - /setwelcome `<text>`: Set a new welcome message. Supports markdown, buttons, and fillings.
 - /resetwelcome: Reset the welcome message.
 - /setgoodbye `<text>`: Set a new goodbye message. Supports markdown, buttons, and fillings.
 - /resetgoodbye: Reset the goodbye message.
-- /cleanservice `<yes/no/on/off>`: Delete all service messages. Those are the annoying 'x joined the group' notifications you see when people join.
-- /cleanwelcome `<yes/no/on/off>`: Delete old welcome messages. When a new person joins, or after 5 minutes, the previous message will get deleted.
+- /cleanservice `<yes/no/on/off>`: ON|OFF Delete all service messages. Those are the annoying 'x joined the group' notifications you see when people join.
+- /cleanwelcome `<yes/no/on/off>`: ON|OFF Delete old welcome messages. When a new person joins, or after 5 minutes, the previous message will get deleted.
 
 **Examples:**
 - Get the welcome message without any formatting
